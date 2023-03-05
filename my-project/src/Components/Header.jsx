2e37598cb2b432230c67b5e86e7604fdb31450e7
Bautista Berto <bertobautista03@gmail.com>
2023-03-04 23:00:04 -0300

    add large screen layout

--- my-project/src/Components/Header.jsx
+++ my-project/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function chooseImage() {
   if (window.innerWidth > 1024) return "./public/Component.svg";
@@ -6,10 +6,24 @@ function chooseImage() {
 }
 
 export default function Header() {
+  const [img, setImg] = useState(chooseImage());
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 1024) {
+        setImg("./public/Component.svg");
+      } else {
+        setImg("./public/ComponentMobile.png");
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+  });
+
   return (
-    <div className="flex flex-col ml-3 w-full">
-      <img className=" xs:ml-auto" src={chooseImage()} alt="" />
-      <p className="text-md tracking-tighter underline underline-offset-2 font-mono text-primary md:text-xl">
+    <div className="flex flex-col ml-3 w-full lg:w-1/6 lg:h-full lg:justify-start lg:items-center gap-0">
+      <img className="xs:ml-auto h-full" src={img} />
+      <p className="text-md tracking-tighter underline underline-offset-2 font-mono text-primary md:text-xl lg:text-center lg:text-2xl">
         I know is trash but fun to me I guess
       </p>
     </div>
--- my-project/src/Components/Home.jsx
+++ my-project/src/Components/Home.jsx
@@ -2,21 +2,59 @@ import React from "react";
 import { Linkedin, Twitter, Github, Spotify } from "react-bootstrap-icons";
 
 export default function Home() {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const date = new Date();
+  const fullDate =
+    monthNames[date.getMonth()] +
+    " " +
+    date.getDate() +
+    " " +
+    date.getFullYear();
+
   return (
-    <section className="mt-6 ml-px my-section">
-      <h1 className="text-8xl md:text-9xl font-black mb-2 font-darkerGrote">
+    <section className="mt-4 ml-px md:mt-3 lg:mt-0 lg:w-full">
+      <h1
+        className="text-8xl md:text-9xl font-black mb-2 font-darkerGrote
+        lg:text-[12rem] lg:mb-0 lg:tracking-wide"
+      >
         SOTD
       </h1>
-      <p className="opacity-80 tracking-normal ml-2 text-md font-mono md:text-xl">
-        Song Of The Day
-        <span className="pl-1 cursor-pointer">🎵</span>
-      </p>
-      <div className="bg-primary-100 my-5 h-96 md:h-[32rem] rounded-xl"></div>
-      <h2 className="text-2xl my-10 tracking-wide uppercase text-center font-bold font-darkerGrote md:text-3xl">
+      <div className="lg:flex lg:flex-row lg:gap-2 lg:justify-between">
+        <p className="opacity-80 tracking-normal ml-2 text-md font-mono md:text-xl lg:text-2xl">
+          Song Of The Day
+          <span className="pl-2 cursor-pointer">🎵 , </span>
+          {fullDate}
+        </p>
+
+        <p className="opacity-70 tracking-normal ml-2 text-md font-mono md:text-xl lg:text-2xl hidden lg:inline">
+          Beta Version 1.0.0
+        </p>
+      </div>
+      <div
+        className="bg-primary-100 my-5 h-96 rounded-xl md:h-[33rem] md:mx-1
+        lg:w-full lg:h-[39rem] lg:flex lg:items-center lg:justify-center lg:py-3 shadow-md shadow-primary-100"
+      >
+        <div className="lg:w-1/2 lg:h-full lg:text-center lg:my-auto"></div>
+      </div>
+      <h2 className="text-2xl my-8 tracking-wide uppercase text-center font-bold font-darkerGrote md:text-3xl md:mt-1 lg:my-10">
         still in development...
       </h2>
-      <div className="w-full px-8">
-        <ul className="flex justify-around items-center">
+      <div className="w-full px-8 md:px-14 lg:px-28 lg:mt-10">
+        <ul className="flex justify-around items-center lg:justify-center lg:gap-20">
           <li className="cursor-pointer">
             <a href="https://twitter.com/tongenjs" target="_blank">
               <Twitter size={32} />
