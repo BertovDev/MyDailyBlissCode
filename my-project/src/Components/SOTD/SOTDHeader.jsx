8ce86ec2130f8aef6a3a656a99aa147b7f364d19
Bautista Berto <bertobautista03@gmail.com>
2023-03-11 14:00:36 -0300

    add basic hover animations

--- my-project/src/Components/SOTD/SOTDHeader.jsx
+++ my-project/src/Components/SOTD/SOTDHeader.jsx
@@ -27,7 +27,7 @@ export default function SOTDHeader() {
   return (
     <div>
       <h1
-        className="text-8xl font-black mb-2 font-darkerGrote md:text-8xl
+        className="text-8xl pl-1 font-black mb-2 font-darkerGrote md:text-8xl
         lg:text-[9rem] lg:mb-0 lg:tracking-wide"
       >
         SOTD
@@ -35,7 +35,16 @@ export default function SOTDHeader() {
       <div className="lg:flex lg:flex-row lg:gap-2 lg:justify-between">
         <p className="opacity-80 tracking-normal ml-2 text-md font-mono md:text-xl lg:text-2xl">
           Song Of The Day
-          <span className="pl-2 cursor-pointer">🎵 , </span>
+          <div class="group relative inline-block">
+            <span className="pl-2 cursor-pointer">🎵 ,</span>
+            <span
+              class="group-hover:opacity-100 group-active:opacity-100 transition-opacity bg-primary-50 px-1 text-sm text-primary-200 font-mono rounded-md absolute
+              -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto w-52 left-8"
+            >
+              Daily Reminder:{" "}
+              <span className="underline underline-offset-2">Be Nice</span>
+            </span>
+          </div>
           {fullDate}
         </p>
         {/* <p className="text-md ml-2 tracking-wide font-mono text-primary lg:text-2xl lg:inline-block">
--- my-project/src/Components/SOTD/YoutubeEmbed.jsx
+++ my-project/src/Components/SOTD/YoutubeEmbed.jsx
@@ -5,11 +5,11 @@ export default function YoutubeEmbed() {
 
   return (
     <div
-      className="bg-primary-100 my-5 h-96 rounded-xl md:h-[33rem] md:mx-1
+      className="bg-gradient-to-r from-primary-100 to-primary-150 my-5 h-96 rounded-xl md:h-[33rem] md:mx-1
     lg:w-11/12 lg:h-full lg:flex lg:items-center lg:justify-center lg:py-3 shadow-md shadow-primary-100"
     >
       <iframe
-        className="w-full h-full p-5 outline-none"
+        className="w-full h-full p-5 lg:py-5 lg:px-12 outline-none"
         src={"https://www.youtube.com/embed/" + embededUrlCode}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
