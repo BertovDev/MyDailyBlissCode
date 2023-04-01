822219d35882117c1c6fa903aee8e52b2fb37e06
BertovDev <bertobautista18@gmail.com>
2023-04-01 14:32:59 -0300

    removed couple things

--- my-project/src/Components/SOTD/SOTDFooter.jsx
+++ my-project/src/Components/SOTD/SOTDFooter.jsx
@@ -4,20 +4,11 @@ import { Linkedin, Twitter, Github, Spotify } from "react-bootstrap-icons";
 export default function SOTDFooter() {
   return (
     <>
-      <div className="w-full px-8 mt-9 md:px-14 lg:px-28 lg:my-6">
-        <ul className="flex justify-around items-center lg:justify-center lg:gap-20">
+      <div className="px-8 w-full  md:px-14 lg:px-28 lg:my-6">
+        <ul className="flex h-32 mt-auto justify-around items-center lg:hidden lg:gap-20">
           <li className="cursor-pointer">
             <a href="https://twitter.com/tongenjs" target="_blank">
               <Twitter size={32} />
-            </a>
-          </li>
-          <li className="cursor-pointer">
-            <a
-              href="https://linkedin.com/in/bautista-berto"
-              target="bl
-            "
-            >
-              <Linkedin size={32} />
             </a>
           </li>
           <li className="cursor-pointer">
@@ -34,9 +25,6 @@ export default function SOTDFooter() {
             </a>
           </li>
         </ul>
-        <h2 className="text-2xl mt-4 tracking-wide uppercase text-center font-bold font-darkerGrote md:text-3xl md:mt-6 lg:mt-6">
-          still in development...
-        </h2>
       </div>
     </>
   );
